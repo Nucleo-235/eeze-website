cd62ad255fc56448d55be688b12debc4e7a0bece
Unknown <luciano@lrnco.cc>
2017-04-17 17:02:21 -0300

    Quem Somos

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
 
   resources :pepsi
+  resources :quem_somos
   resources :projects
   resources :project_contents
   resources :image_project_contents
   resources :text_project_contents
   resources :youtube_project_contents
   resources :vimeo_project_contents
+  post "/contact_form", to: "quem_somos#index"
 
   scope "(:locale)", locale: /pt-BR|en/ do
     root to: 'pages#home'
