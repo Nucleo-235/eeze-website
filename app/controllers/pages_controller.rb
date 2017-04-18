ba33cbdf103fae31a09fcfe3b26ccc2bc856e8ed
Unknown <luciano@lrnco.cc>
2017-04-18 13:03:28 -0300

    Quem somos Desktop

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,9 +5,19 @@ class PagesController < ApplicationController
   before_action :redirect_to_locale_if_not_set, only: [:home]
 
   def home
+    
     @projects = Project.all.order(:sort_order)
     if @can_edit
       @new_project = Project.new
     end
+
+    @contact = ContactForm.new(params[:contact])
+    @contact.request = request
+    if @contact.deliver
+      flash.now[:notice] = 'Thank you for your message. We will contact you soon!'
+    else
+      flash.now[:error] = 'Cannot send message.'
+    end
+
   end
 end
