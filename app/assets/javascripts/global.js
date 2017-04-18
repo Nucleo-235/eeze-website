83a8d8523f508b64bf1bb54cbb7cc737c6a86879
Unknown <luciano@lrnco.cc>
2017-04-17 22:00:53 -0300

    //WIP Home Desktop
    - fadein text
    - carousel
    -  tab

--- app/assets/javascripts/global.js
+++ app/assets/javascripts/global.js
@@ -1,5 +1,17 @@
 function openMenu() {
   var nav = document.querySelector('nav');
-  if (nav.style.margin == '' ) document.querySelector('nav').style.margin = '10px';
+  if (nav.style.margin == '' ) nav.style.margin = '10px';
   else nav.style.margin = '';
 }
+
+$(function() {
+  $('#brand #carousel').carousel({
+    interval: 5000,
+    ride: "carousel"
+  })
+
+  $('.tlt').textillate({ autoStart: false, in: { effect: 'fadeIn', shuffle: true } });
+  $('#brand #carousel').on('slide.bs.carousel', function () {
+    $('.tlt').textillate('start');
+  })
+});
